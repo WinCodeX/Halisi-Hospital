4964b478740f832995376ffdfe2d900258fe9afe
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-03-23 11:39:02 +0300

    Added Footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # Has error [users/signout] then apply or below code for the scope.
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
